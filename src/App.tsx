15c916b54ffba762c8372ce2c946eb561de00ca0
AlmasLiq <bayazinalmas009@gmail.com>
2025-10-12 12:11:30 +0500

    Add initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className="text-12xl font-bold ">Vite + React</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<h1>Route</h1>} />
+        <Route path="/sign-up" element={<h1>Sign Up Page</h1>} />
+        <Route path="/sign-in" element={<h1>Sign in Page</h1>} />
+        <Route path="*" element={<h1>Everything other</h1>} />
+      </Routes>
   )
 }
 
