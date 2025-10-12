edd7d788c3fd1ca8720d52c6c75cf9f4c5769240
AlmasLiq <bayazinalmas009@gmail.com>
2025-10-12 12:36:38 +0500

    Add eslint and pretiier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<h1>Route</h1>} />
-        <Route path="/sign-up" element={<h1>Sign Up Page</h1>} />
-        <Route path="/sign-in" element={<h1>Sign in Page</h1>} />
-        <Route path="*" element={<h1>Everything other</h1>} />
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<h1>Route</h1>} />
+      <Route path="/sign-up" element={<h1>Sign Up Page</h1>} />
+      <Route path="/sign-in" element={<h1>Sign in Page</h1>} />
+      <Route path="*" element={<h1>Everything other</h1>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
